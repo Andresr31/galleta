b16de1d28d37ce90bb2ae653db3d11d766163756
Andres Rojas <carlos.rojasp@autonoma.edu.co>
2018-11-17 10:41:00 -0500

    Creacion crud galleta

--- routes/botman.php
+++ routes/botman.php
@@ -5,14 +5,81 @@ use App\Cookie;
 
 $botman = resolve('botman');
 
-$botman->hears('Romper galleta', function ($bot) {
+$botman->hears('Break cookie', function ($bot) {
 
     $cookie = Cookie::all()->random(1)->first();
 
     $bot->reply($cookie->message);
 });
 
+$botman->hears('create cookie {text}', function ($bot, $text) {
 
+  $cookie = new Cookie();
+  $cookie -> message = $text;
+  $cookie -> save(); 
+
+  $bot->reply('Cookie saved !!');
+});
+
+$botman->hears('Update cookie {id} with {text}', function ($bot, $id, $text) {
+
+  $cookie = Cookie::find($id);
+  if ($cookie == null)
+  {
+    $bot->reply('The cookie dont exist!');
+  }else{
+    $cookie -> message = $text;
+    $cookie -> save(); 
+    $bot->reply('Cookie updated!');
+  }
+});
+
+$botman->hears('1.Find cookie with {text}', function ($bot, $text) {
+
+  $cookies = Cookie::where('message','=', $text)->get();
+
+  foreach($cookies as $cookie)
+  {
+    $bot->reply('Cookie: '. $cookie->id);  
+  }
+  if(count($cookies)==0)
+    $bot->reply('I could not find cookies whit that text');
+  
+});
+$botman->hears('2.Find cookie with as {text}', function ($bot, $text) {
+
+  $cookies = Cookie::where('message','LIKE', "%{$text}%")->get();
+
+  foreach($cookies as $cookie)
+  {
+    $bot->reply('Cookie: '. $cookie->id. ' : '.$cookie->message);  
+  }
+  if(count($cookies)==0){
+    $bot->reply('I could not find cookies whit that text');
+  }
+    
+});
+
+$botman->hears('delete cookie {id}', function ($bot, $id) {
+
+  $control =  Cookie::where('id', '=', $id)->delete();
+  if ($control == 0)
+  {
+    $bot->reply('The cookie dont exist!');
+  }else{ 
+    $bot->reply('The cookie was deleted!');
+  }
+
+  // $cookie = Cookie::find($id);
+  // if ($cookie == null)
+  // {
+  //   $bot->reply('The cookie dont exist!');
+  // }else{
+  //   $cookie -> delete(); 
+  //   $bot->reply('The cookie was deleted!');
+  // }
+    
+});
 //$botman->hears('Hi', function ($bot) {
   //  $bot->reply('Hello!');
 //});
